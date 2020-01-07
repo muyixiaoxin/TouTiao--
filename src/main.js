6f16dceb68d3cc1861f4b6fa36e6f94d2380c083
muyixiaoxin <1015907316@qq.com>
2020-01-07 14:42:46 +0800

    应用vant组件

--- src/main.js
+++ src/main.js
@@ -3,6 +3,14 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import './utils/register-vant.js'
+// import { Cell, CellGroup, Button } from 'vant'
+
+// Vue
+//   .use(Button)
+//   .use(Cell)
+//   .use(CellGroup)
+
 Vue.config.productionTip = false
 
 new Vue({
---
+++ src/utils/register-vant.js
@@ -0,0 +1,6 @@
+import Vue from 'vue'
+import { Button, Cell, CellGroup } from 'vant'
+
+Vue.use(Button)
+  .use(Cell)
+  .use(CellGroup)
